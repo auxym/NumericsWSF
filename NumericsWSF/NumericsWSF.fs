af2cf9e37fe4ae834c85394e31352bb6c7922e03
Francis Therien <frtherien@gmail.com>
2017-12-02 11:19:38 -0500

    Add cubic interpolation function

--- NumericsWSF/NumericsWSF.fs
+++ NumericsWSF/NumericsWSF.fs
@@ -22,3 +22,22 @@ let linInterp
 
     let interp = LinearSpline.Interpolate(x1, y1)
     Array.map interp.Interpolate x2
+
+[<ExcelFunctionDoc(
+    Name="NUM.INTERP.CUBIC",
+    Category="Interpolation",
+    Description="Cubic natural interpolation",
+    Remarks="Based on MathNet.Numerics.CubicSpline"
+    )>]
+let cubicInterp
+        ([<ExcelArgument(Description="x values on which interpolation is based")>]
+        x1:float[])
+
+        ([<ExcelArgument(Description="y values on which interpolation is based")>]
+        y1:float[]) 
+        
+        ([<ExcelArgument(Description="x values at which interpolated values will be calculated")>]
+        x2:float[]) =
+
+    let interp = CubicSpline.InterpolateNatural(x1, y1)
+    Array.map interp.Interpolate x2
