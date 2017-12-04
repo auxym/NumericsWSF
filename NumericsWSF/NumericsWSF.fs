c26720f06220341201e0e43efa773db20e4ef12b
Francis Therien <frtherien@gmail.com>
2017-12-04 17:38:43 -0500

    Add function to reformat 1D array

--- NumericsWSF/NumericsWSF.fs
+++ NumericsWSF/NumericsWSF.fs
@@ -4,12 +4,24 @@ open ExcelDna.Integration
 open ExcelDna.Documentation
 open MathNet.Numerics.Interpolation
 
+/// Reformat 1D array to Nx1 or 1xN 2D array depending on excel caller
+let packForCaller vs =
+    let caller = XlCall.Excel(XlCall.xlfCaller) :?> ExcelReference
+    let rows = caller.RowLast - caller.RowFirst + 1
+    let columns = caller.ColumnLast - caller.ColumnFirst + 1
+
+    match (columns, rows) with
+    | (c, r) when (c >= r)
+        -> Array2D.init 1 (Array.length vs) (fun i j -> vs.[j]) 
+    | _ 
+        -> Array2D.init (Array.length vs) 1 (fun i j -> vs.[i])
+
 [<ExcelFunctionDoc(
     Name="NUM.INTERP.LINEAR",
     Category="Interpolation",
     Description="Linear interpolation",
     Remarks="Based on MathNet.Numerics.LinearSpline"
-    )>]
+)>]
 let linInterp 
         ([<ExcelArgument(Description="x values on which interpolation is based")>]
         x1:float[])
@@ -21,14 +33,14 @@ let linInterp
         x2:float[]) =
 
     let interp = LinearSpline.Interpolate(x1, y1)
-    Array.map interp.Interpolate x2
+    Array.map interp.Interpolate x2 |> packForCaller
 
 [<ExcelFunctionDoc(
     Name="NUM.INTERP.CUBIC",
     Category="Interpolation",
     Description="Cubic natural interpolation",
     Remarks="Based on MathNet.Numerics.CubicSpline"
-    )>]
+)>]
 let cubicInterp
         ([<ExcelArgument(Description="x values on which interpolation is based")>]
         x1:float[])
@@ -40,4 +52,4 @@ let cubicInterp
         x2:float[]) =
 
     let interp = CubicSpline.InterpolateNatural(x1, y1)
-    Array.map interp.Interpolate x2
+    Array.map interp.Interpolate x2 |> packForCaller
